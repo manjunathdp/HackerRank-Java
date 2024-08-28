05aa277024a7f4780b2c7a68e8045ea4808ab8f2
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-08-28 13:26:17 +0530

    Given a string A, print Yes if it is a palindrome, print No otherwise.

--- src/JavaStringReverse.java
+++ src/JavaStringReverse.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String A = sc.next();
-    /* Enter your code here. Print output to STDOUT. */
-    StringBuilder sb = new StringBuilder(A);
-    String rev = sb.reverse().toString();
-    if (A.equals(rev))
-        System.out.println("Yes");
-    else
-        System.out.println("No");
-
+public class JavaStringReverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        StringBuilder sb = new StringBuilder(A);
+        String rev=sb.reverse().toString();
+        if(A.equals(rev))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
 }
